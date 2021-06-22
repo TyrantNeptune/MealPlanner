72e45491d013bb45c7c8563e1bb75635ad764f56
TyrantNeptune <davybaardink@hotmail.com>
2021-06-22 14:47:23 +0200

    First commit

---
+++ mealplanner/src/main/java/YCpowergroup/mealplanner/MealplannerApplication.java
@@ -0,0 +1,13 @@
+package YCpowergroup.mealplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MealplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MealplannerApplication.class, args);
+	}
+
+}
---
+++ mealplanner/src/test/java/YCpowergroup/mealplanner/MealplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package YCpowergroup.mealplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
