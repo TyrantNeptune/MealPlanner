f2e204d4541d14cdec1865cf48c76bfc69ea852a
TyrantNeptune <davybaardink@hotmail.com>
2021-06-25 13:00:59 +0200

    DJR02 first attempt at rest

--- src/main/java/YCpowergroup/mealplanner/rest/RecipeEndpoint.java
+++ src/main/java/YCpowergroup/mealplanner/rest/RecipeEndpoint.java
@@ -16,5 +16,10 @@ public class RecipeEndpoint {
         return recipeService.getAllRecipes();
     }
 
+    /*
+    @GetMapping("found-recipe")
+    public Recipe foundRecipe() { return recipeService.findRecipeByName();}
+     */
+
 
 }
