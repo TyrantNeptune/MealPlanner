3a33aa82be015c8af61e2cd65c388e7cddf7401f
Rickmarges <rickmarges@gmail.com>
2021-06-30 14:35:52 +0200

    Changed properties

--- src/main/java/YCpowergroup/mealplanner/MealplannerApplication.java
+++ src/main/java/YCpowergroup/mealplanner/MealplannerApplication.java
@@ -2,8 +2,10 @@ package YCpowergroup.mealplanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @SpringBootApplication
+@ConfigurationProperties
 public class MealplannerApplication {
 
 	public static void main(String[] args) {
