7bb89bb9e7c69fb7aa2fd246894062d66338e2cf
Shiro <jaron.95@hotmail.com>
2021-07-02 13:32:38 +0200

    Commented out deprecated code

--- src/main/java/YCpowergroup/mealplanner/oud/MealTypeDeprecated.java
+++ src/main/java/YCpowergroup/mealplanner/oud/MealTypeDeprecated.java
@@ -1,12 +1,12 @@
-package YCpowergroup.mealplanner.oud;
-
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public enum MealTypeDeprecated {
-    @JsonProperty("Breakfast")
-    BREAKFAST,
-    @JsonProperty("Lunch")
-    LUNCH,
-    @JsonProperty("Dinner")
-    DINNER
-}
+//package YCpowergroup.mealplanner.oud;
+//
+//import com.fasterxml.jackson.annotation.JsonProperty;
+//
+//public enum MealTypeDeprecated {
+//    @JsonProperty("Breakfast")
+//    BREAKFAST,
+//    @JsonProperty("Lunch")
+//    LUNCH,
+//    @JsonProperty("Dinner")
+//    DINNER
+//}
--- src/main/java/YCpowergroup/mealplanner/oud/RecipeRepositoryDeprecated.java
+++ src/main/java/YCpowergroup/mealplanner/oud/RecipeRepositoryDeprecated.java
@@ -1,13 +1,13 @@
-package YCpowergroup.mealplanner.oud;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Component;
-
-import java.util.List;
-
-@Component
-public interface RecipeRepositoryDeprecated extends CrudRepository<RecipeDeprecated, Long> {
-
-    List<RecipeDeprecated> findAllByNameContaining(String name);
-
-}
+//package YCpowergroup.mealplanner.oud;
+//
+//import org.springframework.data.repository.CrudRepository;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.List;
+//
+////@Component
+//public interface RecipeRepositoryDeprecated extends CrudRepository<RecipeDeprecated, Long> {
+//
+//    List<RecipeDeprecated> findAllByNameContaining(String name);
+//
+//}
