37f75530e905fcf2062f43b9fb592024440f458f
Rickmarges <rickmarges@gmail.com>
2021-07-07 09:15:06 +0200

    Fixed service

--- src/main/java/YCpowergroup/mealplanner/controller/MealPlanService.java
+++ src/main/java/YCpowergroup/mealplanner/controller/MealPlanService.java
@@ -1,7 +1,9 @@
 package YCpowergroup.mealplanner.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MealPlanService {
     @Autowired
     MealPlanRepository mealPlanRepository;
--- src/main/java/YCpowergroup/mealplanner/controller/MealRepository.java
+++ src/main/java/YCpowergroup/mealplanner/controller/MealRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Component;
 
 @Component
 public interface MealRepository extends CrudRepository<Meal, Long> {
-    Meal findMealById(long Id);
+
 }
