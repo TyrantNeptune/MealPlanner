db0f96e1a39590ca3c1853ef2c3fad5e297e048c
Rickmarges <rickmarges@gmail.com>
2021-07-07 12:12:48 +0200

    Added Unit tests and changed workflow to run those tests

--- src/test/java/YCpowergroup/mealplanner/MealplannerApplicationTests.java
+++ src/test/java/YCpowergroup/mealplanner/MealplannerApplicationTests.java
@@ -1,13 +1,31 @@
 package YCpowergroup.mealplanner;
 
+import YCpowergroup.mealplanner.domain.Recipe;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class MealplannerApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private static Recipe testRecipe;
+
+	@BeforeAll
+	static void init() {
+		testRecipe = new Recipe();
+		testRecipe.setName("testing");
+		testRecipe.setServings(4);
 	}
 
+	@Test
+	void recipeNameTest() {
+		assertEquals("testing", testRecipe.getName());
+	}
+
+	@Test
+	void recipeServingsTest() {
+		assertEquals(4, testRecipe.getServings());
+	}
 }
