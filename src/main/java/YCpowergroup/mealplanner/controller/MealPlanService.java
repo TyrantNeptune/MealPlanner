60220c3a7b3ed629160eb4f35ac999ee1c5d3c88
TyrantNeptune <davybaardink@hotmail.com>
2021-07-08 15:45:36 +0200

    allmeals and addmeal

--- src/main/java/YCpowergroup/mealplanner/controller/MealPlanService.java
+++ src/main/java/YCpowergroup/mealplanner/controller/MealPlanService.java
@@ -1,5 +1,6 @@
 package YCpowergroup.mealplanner.controller;
 
+import YCpowergroup.mealplanner.domain.Meal;
 import YCpowergroup.mealplanner.domain.MealPlan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,6 +21,13 @@ public class MealPlanService {
     public MealPlan addMealPlan(MealPlan mealPlan){
         System.out.println("Adding new mealplan...");
         return mealPlanRepository.save(mealPlan);
+    }
+
+    public Iterable<Meal> findAllMeals(){return mealRepository.findAll();}
+
+    public Meal addMeal(Meal meal){
+        System.out.println("Adding new meal...");
+        return mealRepository.save(meal);
     }
 
 }
--- src/main/java/YCpowergroup/mealplanner/rest/MealPlanEndpoint.java
+++ src/main/java/YCpowergroup/mealplanner/rest/MealPlanEndpoint.java
@@ -1,6 +1,7 @@
 package YCpowergroup.mealplanner.rest;
 
 import YCpowergroup.mealplanner.controller.MealPlanService;
+import YCpowergroup.mealplanner.domain.Meal;
 import YCpowergroup.mealplanner.domain.MealPlan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,18 @@ public class MealPlanEndpoint {
     public MealPlan addMealplan(@RequestBody MealPlan mealPlan){
         System.out.println("Adding mealplan starting on "+mealPlan.getStart());
         return mealPlanService.addMealPlan(mealPlan);
+    }
+
+    @GetMapping("allmeals")
+    public Iterable<Meal> allMeals(){
+        System.out.println("Finding all mealplans...");
+        return mealPlanService.findAllMeals();
+    }
+
+    @PostMapping("addmeal")
+    public Meal addMeal(@RequestBody Meal meal){
+        System.out.println("Adding meal on "+meal.getDate()+" to mealplan "+meal.getMealPlan());
+        return mealPlanService.addMeal(meal);
     }
 
 }
