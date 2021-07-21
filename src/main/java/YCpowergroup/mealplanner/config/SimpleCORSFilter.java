38a3c3953d002947a1803750377e67d338c52bbb
Rickmarges <rickmarges@gmail.com>
2021-07-21 16:35:44 +0200

    Trying to fix cache

--- src/main/java/YCpowergroup/mealplanner/config/SimpleCORSFilter.java
+++ src/main/java/YCpowergroup/mealplanner/config/SimpleCORSFilter.java
@@ -21,8 +21,7 @@ public class SimpleCORSFilter implements Filter {
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "36000");
         response.setHeader("Access-Control-Allow-Headers", "origin, content-type, accept");
-        response.setHeader("cache-control", "max-age=31536000");
-        response.setHeader("Content-type", "application/json; charset=utf-8");
+        response.setHeader("Cache-control", "no-cache");
         chain.doFilter(req, res);
     }
 
